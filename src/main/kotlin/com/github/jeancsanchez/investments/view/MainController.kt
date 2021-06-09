bd7bffac98549950759fcfc8e84343cf4d510e29
jeancarlos <jeancs.patricio@gmail.com>
2021-06-09 19:07:17 -0300

    Refatorando para colocar logica de importacao do arquivo CEI em um Service ao invés de diretamente no controller.

--- src/main/kotlin/com/github/jeancsanchez/investments/view/MainController.kt
+++ src/main/kotlin/com/github/jeancsanchez/investments/view/MainController.kt
@@ -4,16 +4,16 @@ import com.github.jeancsanchez.investments.data.CorretoraRepository
 import com.github.jeancsanchez.investments.data.OperacaoRepository
 import com.github.jeancsanchez.investments.data.PapelRepository
 import com.github.jeancsanchez.investments.domain.RelatorioService
-import com.github.jeancsanchez.investments.domain.model.*
+import com.github.jeancsanchez.investments.domain.model.Operacao
 import com.github.jeancsanchez.investments.domain.model.dto.ConsolidadoDTO
-import org.apache.poi.hssf.usermodel.HSSFWorkbook
+import com.github.jeancsanchez.investments.view.services.ApplicationService
+import com.github.jeancsanchez.investments.view.services.CEIxlsImporterService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
-import java.nio.file.Files
-import java.time.LocalDate
 
 /**
  * @author @jeancsanchez
@@ -25,10 +25,9 @@ import java.time.LocalDate
 @Configuration
 @RequestMapping("/api")
 class MainController(
-    @Autowired val corretoraRepository: CorretoraRepository,
     @Autowired val operacaoRepository: OperacaoRepository,
-    @Autowired val papelRepository: PapelRepository,
-    @Autowired val relatorioService: RelatorioService
+    @Autowired val relatorioService: RelatorioService,
+    @Autowired val ceiXLSImporterService: CEIxlsImporterService,
 ) {
     @CrossOrigin(origins = ["http://localhost:3000"])
     @GetMapping("/operacoes")
@@ -45,90 +44,7 @@ class MainController(
     @CrossOrigin(origins = ["http://localhost:3000"])
     @PostMapping("/sync")
     fun syncOperacoes(@RequestParam("arquivo") arquivo: MultipartFile): ResponseEntity<List<Operacao>> {
-        val resultList = mutableListOf<Operacao>()
-
-        val workbook = HSSFWorkbook(arquivo.inputStream)
-        val currentSheet = workbook.getSheetAt(0)
-        val linhaCabecalhos = 10
-        val colunaDataOperacao = 1
-        val colunaTipoOperacao = 3
-        val colunaCodenamePapel = 6
-        val colunaTipoDoPapel = 7
-        val colunaQuantidade = 8
-        val colunaPrecoOperacao = 9
-
-        val nomeDaCorretora = currentSheet.getRow(9).getCell(1).stringCellValue
-        var dataOperacao: LocalDate? = null
-        var tipoOperacao: TipoOperacao? = null
-        var tipoDeLote: String = TipoDeLote.FRACIONARIO
-        var papelCodename = ""
-        var tipoPapel: TipoAcao? = null
-        var quantidade = 0
-        var preco = 0.0
-
-        currentSheet.rowIterator().withIndex().forEach rowsLooping@{ row ->
-            if (row.index > linhaCabecalhos) {
-                row.value.cellIterator().forEach columnsLooping@{ column ->
-                    when (column.columnIndex) {
-                        colunaDataOperacao -> {
-                            dataOperacao = column.stringCellValue.formatStringBRToDate() ?: let {
-                                return@rowsLooping
-                            }
-                        }
-                        colunaTipoOperacao -> {
-                            tipoOperacao = column.stringCellValue.extractTipoOperacao()
-                        }
-                        colunaCodenamePapel -> {
-                            papelCodename = column.stringCellValue.extractPapelName()
-                            tipoDeLote = column.stringCellValue.extractTipoDeLote()
-                        }
-                        colunaTipoDoPapel -> {
-                            tipoPapel = column.stringCellValue.extractTipoAcao()
-                        }
-                        colunaQuantidade -> {
-                            quantidade = column.numericCellValue.toInt()
-                        }
-                        colunaPrecoOperacao -> {
-                            preco = column.numericCellValue
-                        }
-                    }
-                }
-
-                val papel =
-                    papelRepository.findTop1ByNomeIgnoreCase(papelCodename)
-                        ?: papelRepository.save(Papel(codigo = papelCodename))
-                val corretora = corretoraRepository.findTop1ByNomeIgnoreCase(nomeDaCorretora)
-                    ?: corretoraRepository.save(Corretora(nome = nomeDaCorretora))
-
-                val newOperacao = Operacao(
-                    data = dataOperacao!!,
-                    papel = papel,
-                    corretora = corretora,
-                    tipoDaOperacao = tipoOperacao!!,
-                    tipoDaAcao = tipoPapel!!,
-                    tipoDeLote = tipoDeLote,
-                    preco = preco,
-                    quantidade = quantidade
-                )
-
-                resultList.add(newOperacao)
-            }
-        }
-
-        if (resultList.isNotEmpty()) {
-            if (operacaoRepository.count() == 0L) {
-                operacaoRepository.saveAll(resultList)
-                return ResponseEntity.ok(resultList)
-            }
-
-            if (resultList.last().hashId !== operacaoRepository.findTopByOrderByIdDesc()?.hashId) {
-                val lastIndexFromDB = operacaoRepository.count() - 1
-                val differenceList = resultList.subList(lastIndexFromDB.toInt(), resultList.size - 1)
-                operacaoRepository.saveAll(differenceList)
-                return ResponseEntity.ok(resultList)
-            }
-        }
-
-        return ResponseEntity.ok(emptyList())
+        val result = ceiXLSImporterService.run(arquivo)
+        return ResponseEntity.ok(result)
     }
 }
